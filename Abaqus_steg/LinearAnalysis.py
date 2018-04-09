aa251f3e27505109fe86c231f0016832fd850d67
SondreRokvam <36338470+SondreRokvam@users.noreply.github.com>
2018-04-09 14:43:23 +0200

    Sette på simulareing

--- Abaqus_steg/LinearAnalysis.py
+++ Abaqus_steg/LinearAnalysis.py
@@ -63,8 +63,9 @@ def get_stiffness():
     g = open(lagrestiffpath, "a")
     #g = open(lagrestiffpath, "a")
     print '\nStiffnessmatrix found\n'
+    g.write(str(FaktiskVf) + '\t')
     for a in range(0, 6):
-        g.write(str(FaktiskVf)+'\t'+str(float(stiffmatrix[0][a]))+'\t'+str(float(stiffmatrix[1][a]))+'\t'+str(float(stiffmatrix[2][a]))+'\t'+str(float(stiffmatrix[3][a]))+'\t'+str(float(stiffmatrix[4][a]))+'\t'+str(float(stiffmatrix[5][a])))
+        g.write(str(float(stiffmatrix[0][a]))+'\t'+str(float(stiffmatrix[1][a]))+'\t'+str(float(stiffmatrix[2][a]))+'\t'+str(float(stiffmatrix[3][a]))+'\t'+str(float(stiffmatrix[4][a]))+'\t'+str(float(stiffmatrix[5][a])))
         if not a==5:
             g.write('\t\t')
         print '%7f \t %7f \t %7f \t %7f \t %7f \t %7f' % (stiffmatrix[0][a], stiffmatrix[1][a], stiffmatrix[2][a], stiffmatrix[3][a], stiffmatrix[4][a], stiffmatrix[5][a])

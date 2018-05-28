0c44c2efbba9244d8917d963a60da3e29e024dc8
SondreRokvam <36338470+SondreRokvam@users.noreply.github.com>
2018-05-28 02:54:34 +0200

    Tweaks

--- BatchLauchFile.bat
+++ BatchLauchFile.bat
@@ -1,5 +1,4 @@
 C:\Users\Sondre\AppData\Local\Programs\Python\Python36-32\pythonw.exe C:\MultiScaleMethod\Github\Multiscale-Modeling\Stotte\Prepare_test.py %*
 chdir C:\Temp
-pause
 "C:\SIMULIA\Abaqus\Commands\abq6144.bat" cae noGUI="C:\MultiScaleMethod\Github\Multiscale-Modeling\MicroscaleModelling_randomSweep.py"
 pause
